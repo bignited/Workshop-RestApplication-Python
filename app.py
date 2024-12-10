7f76a833bdcbebb084e61a9e824a6eb1538ef16d
KoenVanBelle <koen.vanbelle@bignited.be>
2024-12-10 08:47:31 +0100

    Feat: Add friend endpoint

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+friends = []   
+
 @app.route('/hello', methods=['GET'])
 def hello_world():
     return "Hello, World!"
@@ -11,6 +13,18 @@ def hello_name():
     data = request.get_json()
     name = data.get('name', 'World')
     return jsonify(message=f"Hello, {name}")
+
+@app.route('/friends', methods=['GET'])
+def hello_friends():
+    return friends
+
+@app.route('/friend', methods=['POST'])
+def new_friends():
+    data = request.get_json()
+    name = data.get('name')
+    friends.append(name)
+    return jsonify(message=f"Welcome, {name}")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
